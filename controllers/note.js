d3cffcd5fb00a692c52cb8af413d2ac6aa2d62a0
Bob Ross <51304258+happyliltrees@users.noreply.github.com>
2019-11-14 19:54:31 -0500

    will it work on heroku NOW?

--- controllers/note.js
+++ controllers/note.js
@@ -1,21 +1,20 @@
-// Controller for our notes
-// ========================
+
 var db = require("../models");
 
 module.exports = {
-  // Find one note
+  // Find single note
   find: function(req, res) {
     db.Note.find({ _headlineId: req.params.id }).then(function(dbNote) {
       res.json(dbNote);
     });
   },
-  // Create a new note
+  // Create new note
   create: function(req, res) {
     db.Note.create(req.body).then(function(dbNote) {
       res.json(dbNote);
     });
   },
-  // Delete a note with a given id
+  // Delete a note with a specific id
   delete: function(req, res) {
     db.Note.remove({ _id: req.params.id }).then(function(dbNote) {
       res.json(dbNote);
